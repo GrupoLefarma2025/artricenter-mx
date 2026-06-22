b6e57b5e726e96f6a67275a267b527a92ae57dab
Brandom Ledesma <brandom.ledesma@hotmail.com>
2026-06-22 22:25:52 +0000

    Add new team member image for Dr. Arce in WebP format

--- src/data/branches.ts
+++ src/data/branches.ts
@@ -11,7 +11,7 @@ export const branches: Branch[] = [
   {
     id: "la-raza",
     name: "La Raza",
-    logo: "/assets/images/branches/la-raza.png",
+    logo: "/assets/images/branches/la-raza.webp",
     address: "Calzada Vallejo 233<br>Héroes de Nacozári, GAM<br>07780 CDMX",
     phone: "55 5989 0607",
     mapsUrl: "https://www.google.com/maps/search/?api=1&query=Artricenter+La+Raza"
@@ -19,7 +19,7 @@ export const branches: Branch[] = [
   {
     id: "atizapan",
     name: "Atizapán",
-    logo: "/assets/images/branches/atizapan.png",
+    logo: "/assets/images/branches/atizapan.webp",
     address: "Boulevard Adolfo López Mateos No. 65,<br>Manzana 1, Lote 16, Zona 8,<br>El Potrero, 57975,<br>Ex-Ejido de Atizapán III, C.P. 57975",
     phone: "55 5989 0607",
     mapsUrl: "https://goo.gl/maps/pAipxVAepPY7jqG48"
@@ -27,7 +27,7 @@ export const branches: Branch[] = [
   {
     id: "viaducto",
     name: "Viaducto",
-    logo: "/assets/images/branches/viaducto.png",
+    logo: "/assets/images/branches/viaducto.webp",
     address: "Viaducto Río de la Piedad 130<br>Jamaica, Venustiano Carranza<br>15800 CDMX",
     phone: "55 5989 0607",
     mapsUrl: "https://www.google.com/maps/search/?api=1&query=Artricenter+Viaducto"
